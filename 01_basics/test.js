9a74a1bb45f04864b2a250a6dc5425ac8e43e04a
abul-hasan-farrukh <151547767+abul-hasan-farrukh@users.noreply.github.com>
2025-03-01 16:57:32 +0000

    made edit in test.js file

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,5 @@
-console.log("Hasan");
+console.log("Founder of JS - Brendan Eich");
+console.log("Node.js (2009) & Deno (2018) was founded by Ryan Dahl");
+console.log("It was created in 10 days in 1995.");
+console.log("JavaScript was originally called Mocha, then LiveScript");
+console.log("The JavaScript trademark was originally issued to Sun Microsystems in 1997, and was transferred to Oracle in 2009");
