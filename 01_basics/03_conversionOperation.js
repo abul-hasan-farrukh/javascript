f3f498d759dfbca55d5a2eee32b56195fd96e1b3
abul-hasan-farrukh <151547767+abul-hasan-farrukh@users.noreply.github.com>
2025-03-06 11:47:27 +0000

    conversion of datatypes in JS

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let marks = "33abc"        
+
+console.log(typeof marks);
+console.log(typeof (marks));
+
+let valueInNumber = Number(marks)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // NaN i.e. not a number
+
+// "33" => 33
+// "34abs" => NaN
+// true = 1; false = 0
+// undefined => NaN
+// null => 0     
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// " " => false
+// "hitesh" => true
+
+let Number = 33 
+
+let stringNumber = String(Number)
+console.log(stringNumber)
+console.log(type stringNumber);
+
