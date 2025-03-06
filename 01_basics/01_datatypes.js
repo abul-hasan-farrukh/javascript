59079e31314de669d690c204b54b4a202354ce1a
abul-hasan-farrukh <151547767+abul-hasan-farrukh@users.noreply.github.com>
2025-03-06 11:13:59 +0000

    datatypes in JS

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version of JS.
+
+//  alert(4-6) // this will not run because we are using node.js, not web browser.
+
+console.log(3 
+    + 
+    9
+); // code readability should be high
+
+let name = "Hasan"
+let age = 21 
+let isLoggedIn = true;
+let state;
+
+//Primitive Data Types of JS.
+// number => 2 to power 53
+// string => ""
+// boolean = true/false
+// null => standalone value, it is an object.
+// undefined =>  when we dont assign value to a variable
+// symbol = for uniqueness, widely used in React.js to diffentiate between components.
+
+// object
+
+console.log(typeof undefined); // OUTPUT: undefined
+console.log(typeof null); // OUTPUT: object
+
+
+
+
