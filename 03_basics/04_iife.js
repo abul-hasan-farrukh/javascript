aaede149016cc0f74abda7cc4cdb7446d276aa0a
abul-hasan-farrukh <151547767+abul-hasan-farrukh@users.noreply.github.com>
2025-04-14 17:30:34 +0000

    immediately invoked function expression (IIFE) in JS.

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression (IIFE)
+
+//IIFE is used to remove the pollution caused by variables in global scope. These function get executed immediately.
+//NOTE: When two IIFE are declared together then we have to stop the execution of 1st using ;    
+
+(function tea () { //This is named (tea) IIFE
+    console.log(`Database Connected`);    
+} ) (); //calling function immediately by wrapping the function in parentheses() .
+
+( (name) => {
+    console.log(`DB Connected ${name}`);    
+} ) (`Hasan`)
+
+
