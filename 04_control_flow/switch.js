815f6a00c5ec77bd8eba974537d0c1f0973adc66
abul-hasan-farrukh <151547767+abul-hasan-farrukh@users.noreply.github.com>
2025-04-25 16:17:12 +0000

    switch case in JS.

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,57 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");        
+        break;
+    case 2:
+        console.log("February");        
+        break;
+    case 3:
+        console.log("March");        
+        break;
+    case 4:
+        console.log("April");        
+        break;
+    case 5:
+        console.log("May");        
+        break;
+
+    default:
+        console.log("Invalid Month Number");        
+        break;
+}
+
+
+const months = "March"
+
+switch (months) {
+    case "january":
+        console.log("January");        
+        break;
+    case "february":
+        console.log("February");        
+        break;
+    case "March":
+        console.log("March");        
+        break;
+    case "April":
+        console.log("April");        
+        break;
+    case "May":
+        console.log("May");        
+        break;
+
+    default:
+        console.log("Invalid Month Number");        
+        break;
+}
