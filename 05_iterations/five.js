c08f40c48072aaaa895fd18a407e02081afdf49e
abul-hasan-farrukh <151547767+abul-hasan-farrukh@users.noreply.github.com>
2025-04-26 10:49:55 +0000

    for-each loop in JS.

---
+++ 05_iterations/five.js
@@ -0,0 +1,42 @@
+//for-each loop
+
+const coding = ["java", "ruby", "python"]
+
+// coding.forEach( function (item) {
+//     console.log(item);    
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);    
+// } )
+
+function printMe(item) {
+    console.log(item);   
+}
+
+coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);    
+})
+
+const mycoding = [
+    {
+        languageName: "JavaScript",
+        languageFile: "js"
+    },
+
+    {
+        languageName: "Java",
+        languageFile: "java"
+    },
+
+    {
+        languageName: "Python",
+        languageFile: "py"
+    }
+]
+
+mycoding.forEach( (item) => {
+    console.log(item.languageFile);    
+})
